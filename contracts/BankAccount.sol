47aa1064c06025f79cf85f47f0de6c6c26682bb6
aditya gaargya <adityagaargya@192.168.1.31>
2023-01-14 20:24:45 +0530

    Deposit and createAccount function

--- contracts/BankAccount.sol
+++ contracts/BankAccount.sol
@@ -37,11 +37,60 @@ contract BankAccount {
     uint nextAccountId;
     uint nextWithdrawId;
 
-    function deposit(uint accountId) external payable {}
+    modifier accountOwner(uint accountId) {
+        bool isOwner;
+        for (uint i; i < accounts[accountId].owners.length; i++) {
+            if (accounts[accountId].owners[i] == msg.sender) {
+                isOwner = true;
+                break;
+            }
+        }
+        require(isOwner, "You are not the owner of this account");
+        _;
+    }
 
-    function createAccount(address[] calldata otherOwners) external {}
+    modifier validOwners(address[] calldata owners) {
+        require(owners.length + 1 <= 3, "Maximum of 3 owners per account");
+        for (uint i; i < owners.length; i++) {
+            for (uint j = i + 1; j < owners.length; j++) {
+                if (owners[i] == owners[j]) {
+                    revert("No duplicate owners");
+                }
+            }
+        }
+        _;
+    }
 
-    function requestWithdrawal(uint accountId, uint amount) external {}
+    function deposit(uint accountId) external payable accountOwner(accountId) {
+        accounts[accountId].balance += msg.value;
+    }
+
+    function createAccount(
+        address[] calldata otherOwners
+    ) external validOwners(otherOwners) {
+        address[] memory owners = new address[](otherOwners.length + 1);
+        owners[otherOwners.length] = msg.sender;
+
+        uint id = nextAccountId;
+
+        for (uint i; i < owners.length; i++) {
+            if (i < owners.length - 1) {
+                owners[i] = otherOwners[i];
+            }
+            if (userAccounts[owners[i]].length > 2) {
+                revert("Each user can have a max of only 3 Accounts");
+            }
+            userAccounts[owners[i]].push(id);
+        }
+        accounts[id].owners = owners;
+        nextAccountId++;
+        emit AccountCreated(owners, id, block.timestamp);
+    }
+
+    function requestWithdrawal(
+        uint accountId,
+        uint amount
+    ) external accountOwner(accountId) {}
 
     function approveWithdrawal(uint accountId, uint withdrawId) external {}
 
